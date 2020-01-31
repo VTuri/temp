7c1cec4dfe4ecd46dc23d97fcd93c3288cf43e69
Vajk Turi <turi.vajk@gmail.com>
2020-02-01 00:59:12 +0100

    Caching problem solved

--- app/app.py
+++ app/app.py
@@ -1,28 +1,30 @@
 from flask import Flask, jsonify, abort, make_response, request
 from main import Scrape
 
+
 app = Flask(__name__)
 
 
-@app.route('/scrape/', methods=['GET'])
+@app.route('/scrape/')
 def get_data():
     """
     Example:
     /scrape/?number=100&from=01/02/2020&to=10/02/2020
     :return: json scraped data
     """
-    result=[]
     item_number = request.args.get("number")
     from_date = request.args.get("from")
     to_date = request.args.get("to")
     scrape = Scrape(numbers_per_page=item_number, from_date=from_date, to_date=to_date)
     scrape.startSelenium()
+    #Without this the API stores the previous result and it returns is
+    scrape.clear()
     scraped_list = scrape.parseData()
     result = scraped_list.copy()
-    scrape.clear()
+    scraped_list.clear()
     scrape.quit()
     return jsonify(result)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)

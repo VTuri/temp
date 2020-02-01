8b0779081ad0b880f98c12ab89df19c5ebe0d7e4
Vajk Turi <turi.vajk@gmail.com>
2020-02-01 01:23:58 +0100

    slight code change for better readability

--- app/app.py
+++ app/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, jsonify, abort, make_response, request
 from main import Scrape
 
-
 app = Flask(__name__)
 
 
@@ -15,13 +14,15 @@ def get_data():
     item_number = request.args.get("number")
     from_date = request.args.get("from")
     to_date = request.args.get("to")
+
     scrape = Scrape(numbers_per_page=item_number, from_date=from_date, to_date=to_date)
     scrape.startSelenium()
-    #Without this the API stores the previous result and it returns is
+    # Without this the API stores the previous result and it returns is
     scrape.clear()
+
     scrape.parseData()
     scrape.clean_data()
-    scraped_list =scrape.get_data()
+    scraped_list = scrape.get_data()
     result = scraped_list.copy()
     scraped_list.clear()
     scrape.quit()
